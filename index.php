5cdb2004be4f285808e2206c9fd9efc29658b83c
Chris Yang <cyang287@yahoo.com>
2019-04-09 13:48:14 -0400

    finished html markup

---
+++ index.php
@@ -0,0 +1,292 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>LaGrange, TX</title>
+	<meta name="description" content="">
+	<meta name="robots" content="index, follow">
+
+	<link rel="stylesheet" href="_assets_/plugins/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Roboto:700">
+	<link rel="stylesheet" href="_assets_/css/layout.css">
+
+	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
+	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="_assets_/images/touch-icon-ipad.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="_assets_/images/touch-icon-iphone4.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="_assets_/images/touch-icon-ipad2.png">
+
+	<!--[if !IE]><!-->
+	<link rel="stylesheet" href="_assets_/plugins/add-to-homescreen/style/addtohomescreen.css">
+	<script src="_assets_/plugins/add-to-homescreen/src/addtohomescreen.min.js"></script>
+	<script>addToHomescreen();</script>
+	<!--<![endif]-->
+
+	<!-- Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body id="homepage">
+
+<header>
+	<a href="#main" id="skip" tabindex="0">SKIP TO CONTENT</a>
+
+	<div class="container">
+		<a href="./" id="logo" class="clearfix">
+			<img src="./_assets_/images/logo.png" alt="Logo">
+		</a><!-- /.logo -->
+
+		<div id="toggles" class="hidden-lg hidden-md">
+			<div tabindex="0" id="search-toggle-mobile" class="fa fa-search"></div>
+			<div tabindex="0" id="nav-toggle" class="fa fa-bars"></div>	
+		</div><!-- /#toggles -->
+
+		<nav>
+			<ul id="nav">
+				<li><a href="./">home</a></li>
+				<li>
+					<a href="./">departments</a>
+					<ul>
+						<li><a href="./">malesuada sollicitudin</a></li>
+						<li><a href="./">vestibulum ipsum</a></li>
+						<li>
+							<a href="./">adipiscing ipsum</a>
+							<ul>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+								<li><a href="./">this is a link</a></li>
+							</ul>
+						</li>
+						<li><a href="./">adipiscing ligula ornare</a></li>
+						<li><a href="./">ligula dolor justo</a></li>
+						<li><a href="./">malesuada tortor tristique</a></li>
+						<li><a href="./">vehicula bibendum</a></li>
+						<li><a href="./">inceptos pellentesque</a></li>
+						<li><a href="./">purus magna dolor</a></li>
+						<li><a href="./">vehicula venenatis mattis</a></li>
+					</ul>
+				</li>
+				<li><a href="./">goverment</a></li>
+				<li><a href="./">residents</a></li>
+				<li><a href="./">business</a></li>
+				<li><a href="./">visitor information</a></li>
+			</ul> <!-- /#nav -->
+		</nav>
+
+		<div class="weather hidden-sm hidden-xs">
+			<div id="weather"></div><!-- /#weather -->
+		</div><!-- /.weather -->	
+
+		<div id="search">
+			<form class="search-form" method="get" action="search.php">
+				<label class="fa fa-search" for="search-input"></label>
+				<input name="q" class="form-control search-input" placeholder="SEARCH" type="search" id="search-input">
+				<button><i class="fa fa-search"></i></button>
+			</form><!-- /.search-form -->
+		</div><!-- /#search -->
+	</div><!-- /.container -->
+</header><!-- /.clearfix -->
+
+<main id="main">
+	<section id="content">
+		<section id="welcome">
+			<div id="welcome-info" class="clearfix">
+				<h1><span>welcome to</span>la grange</h1>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. 
+				</p>
+			</div><!-- /#welcome-info -->
+			<div id="welcome-bg" style="background:url('./_assets_/images/welcome-bg.jpg') center no-repeat;background-size:cover;"></div>
+		</section><!-- /#welcome -->
+
+		<section id="quick-links">
+			<div id="quick-links-container-wrapper">
+				<div class="container">
+					<div id="quick-links-wrapper">
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql1.png') center no-repeat;background-size:cover;"></div>
+							<h3>pay utility bill</h3>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql2.png') center no-repeat;background-size:cover;"></div>
+							<h3>pay court fine</h3>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql3.png') center no-repeat;background-size:cover;"></div>
+							<h3>report a concern</h3>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql4.png') center no-repeat;background-size:cover;"></div>
+							<h3>career opportnities</h3>
+						</a><!-- /.quick-links -->
+						<a href="./" class="quick-link">
+							<div class="quick-link-icon" style="background:url('./_assets_/images/ql5.png') center no-repeat;background-size:cover;"></div>
+							<h3>public notices</h3>
+						</a><!-- /.quick-links -->
+					</div><!-- /#quick-links-wrapper.owl-carousel -->
+				</div><!-- /.container -->
+			</div><!-- /#quick-links-container-wrapper -->
+		</section><!-- /#quick-links -->
+
+		<section id="news">
+			<h2>recent news</h2>
+			<div id="news-links" class="owl-carousel">
+				<div class="news-link">
+					<div class="news-link-img" style="background:url('./_assets_/images/news1.jpg') center no-repeat;background-size:cover;"></div>
+					<div class="news-link-info">
+						<span>february 3, 2019</span>
+						<h3>sem tellus elit consectetur adipiscing</h3>
+						<p>
+							Nullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque
+						</p>
+						<a href="./">read more &rarr;</a>
+					</div><!-- /.news-link-info -->
+				</div><!-- /.news-link -->
+				<div class="news-link">
+					<div class="news-link-img" style="background:url('./_assets_/images/news2.jpg') center no-repeat;background-size:cover;"></div>
+					<div class="news-link-info">
+						<span>march 5, 2019</span>
+						<h3>news post title inceptos mattis fusce elit bibendum</h3>
+						<p>
+							Maecenas sed diam eget risus varius blandit sit amet non magna. Integer posuere erat a ante venenatis dapibus posuere velit aliquet...
+						</p>
+						<a href="./">read more &rarr;</a>
+					</div><!-- /.news-link-info -->
+				</div><!-- /.news-link -->
+				<div class="news-link">
+					<div class="news-link-img" style="background:url('./_assets_/images/news3.jpg') center no-repeat;background-size:cover;"></div>
+					<div class="news-link-info">
+						<span>march 1, 2019</span>
+						<h3>vulputate pellentesque cras dolor inceptos</h3>
+						<p>
+							Donec id elit non mi porta gravida at eget metus. Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu leo. Cras mattis consectetur...
+						</p>
+						<a href="./">read more &rarr;</a>
+					</div><!-- /.news-link-info -->
+				</div><!-- /.news-link -->
+			</div><!-- /#news-links -->
+			<a href="./" class="btn">see all news</a>
+		</section><!-- /#news -->
+
+		<section id="events">
+			<div id="events-bg" style="background:url('./_assets_/images/events-bg.png') center no-repeat;background-size:cover;"></div>
+			<h2>upcoming events</h2>
+			<div><img src="./_assets_/images/events.png" alt="Events"></div>
+			<a href="./" class="btn">see all events</a>
+		</section><!-- /#events -->
+
+		<section id="social">
+			<h2>we are social</h2>
+			<div class="social-links hidden-sm hidden-xs">
+				<a href="./" class="fa fa-facebook"></a>
+				<a href="./" class="fa fa-twitter"></a>
+				<a href="./" class="fa fa-instagram"></a>
+				<a href="./" class="fa fa-youtube"></a>
+			</div><!-- /.social-links -->
+			SOCIAL FEED AND STUFF HERE
+		</section><!-- /#social -->
+	</section><!-- /#content -->
+</main>
+
+<footer>
+	<section id="revize-logo">
+		<div class="container">
+			<span>&copy; Copyright City of La Grange, TX</span>
+			<span>Powered by <a href="https://www.revize.com/">revize</a>, The Government Website Experts. <a href="./">login</a></span>
+		</div><!-- /.container -->
+	</section><!-- /#revize-logo -->
+
+	<section id="contact">
+		<span>155 E. Colorado St. La Grange, TX 78945</span>
+		<span>979.968.5805</span>
+	</section>
+</footer>
+
+<div class="alert alert-dismissible floating-alert fade in os-animation" data-os-animation="fadeInUpBig" data-os-animation-delay="2s" role="alert">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	<h4>Alert Title 30 characters Max</h4>
+	<p>Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120 characters Max 120</p>
+	<p><a href="#" class="btn">Read More</a></p>
+</div><!-- /.alert -->
+
+
+<!-- Share widget make into an include file -->
+<button type="button" class="share-btn floating-share-btn" data-toggle="modal" data-target="#shareModal">
+	<i class="fa fa-share-alt"></i>
+	<span>Share</span>
+</button>
+
+<div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="shareModal">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Share this page</h4>
+			</div>
+			<div class="modal-body">
+				<div class="copylink">
+					<p>Copy and paste this code into your website.</p>
+					<pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+				</div><!-- /.copylink -->
+				<div class="share-btns">
+					<p>Share this page on your favorite Social network</p>
+					<div class="row">
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-facebook"></i> Facebook
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-twitter"></i> Twitter
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-google-plus"></i> Google Plus
+							</a>
+						</div>
+						<div class="col-md-3 col-xs-6">
+							<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+							target="_blank">
+								<i class="fa fa-reddit"></i> Reddit
+							</a>
+						</div>
+					</div><!-- /.row -->
+				</div><!-- /.share-btns -->
+				<button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+			</div><!-- /.modal-body -->
+		</div>
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- Share widget make into an include file -->
+
+
+<script src="_assets_/js/jquery.min.js"></script>
+<script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
+<script src="_assets_/plugins/owl.carousel/owl-carousel/owl.carousel.min.js"></script>
+<script src="_assets_/plugins/jquery.bxslider/jquery.bxslider.min.js"></script>
+<script src="_assets_/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script src="_assets_/plugins/twitter/js/jquery.tweet.min.js"></script>
+<script src="_assets_/plugins/instafeed/instafeed.min.js"></script>
+<script src="_assets_/plugins/sticky/jquery.sticky.js"></script>
+<script src="_assets_/plugins/matchHeight/dist/jquery.matchHeight-min.js"></script>
+<script src="_assets_/plugins/revizeWeather/js/revizeWeather.min.js"></script>
+<script src="_assets_/plugins/waypoints/waypoints.min.js"></script>
+<script src="_assets_/js/scripts.js"></script>
+
+</body>
+</html>
